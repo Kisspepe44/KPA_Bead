74a75ca6252b43f1714d7368f50b2c8c6a5a7d00
Kiss Péter Attila <kisspepe2002@gmail.com>
2020-10-25 21:45:54 +0100

    2feladat 1.1

--- app/src/main/java/com/example/kpa_bead/MainActivity.java
+++ app/src/main/java/com/example/kpa_bead/MainActivity.java
@@ -3,12 +3,46 @@ package com.example.kpa_bead;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
 
 public class MainActivity extends AppCompatActivity {
+    Button btn_ko,btn_papir,btn_ollo;
+    ImageView sajat,elenfel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn_ko = (Button) findViewById(R.id.btn_ko);
+        btn_papir = (Button) findViewById(R.id.btn_papir);
+        btn_ollo = (Button) findViewById(R.id.btn_ollo);
+
+        sajat = (ImageView) findViewById(R.id.sajat);
+        elenfel = (ImageView) findViewById(R.id.elenfel);
+
+        btn_ko.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sajat.setImageResource(R.drawable.rock);
+            }
+        });
+        btn_papir.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sajat.setImageResource(R.drawable.paper);
+            }
+        });
+        btn_ollo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sajat.setImageResource(R.drawable.scissors);
+            }
+        });
+
     }
 }
